fc1d62baa30dc0e02cbdbba34ac748a2fe258e27
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-09-24 12:46:31 -0300

    feat(telescope): changed telescope keymaps

--- init.lua
+++ init.lua
@@ -187,16 +187,17 @@ require('lazy').setup({
 
       -- TODO there's this thing where I move between splits with WASD so this telescope wont work form me...
 
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Search [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>tb', builtin.builtin, { desc = '[T]elescope list [B]uiltins' })
+      vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = '[T]elescope Search [H]elp' })
+      vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = '[T]elescope Search [K]eymaps' })
+      vim.keymap.set('n', '<leader>ts', builtin.builtin, { desc = '[T]elescope Search [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>tw', builtin.grep_string, { desc = '[T]elescope Search current [W]ord' })
+      vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = '[T]elescope Search by [G]rep' })
+      vim.keymap.set('n', '<leader>td', builtin.diagnostics, { desc = '[T]elescope Search [D]iagnostics' })
+      vim.keymap.set('n', '<leader>tr', builtin.resume, { desc = '[T]elescope Search [R]esume' })
+      vim.keymap.set('n', '<leader>t.', builtin.oldfiles, { desc = '[T]elescope Search Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Buffers List' })
       vim.keymap.set('n', '<leader>bl', builtin.buffers, { desc = '[B]uffers [L]ist' })
 
       -- Slightly advanced example of overriding default behavior and theme
