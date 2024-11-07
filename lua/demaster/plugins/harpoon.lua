860bd14b6237a7d1527d360f90a68cf5d378fde4
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-11-07 18:35:15 -0300

    feat(plugins): harpoon can open splits now

--- lua/demaster/plugins/harpoon.lua
+++ lua/demaster/plugins/harpoon.lua
@@ -4,6 +4,18 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function ()
     local harpoon = require("harpoon")
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true
+      }
+    })
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function() harpoon.ui:select_menu_item({ vsplit = true }) end, { buffer = cx.bufnr })
+        vim.keymap.set("n", "<C-x>", function() harpoon.ui:select_menu_item({ split = true }) end, { buffer = cx.bufnr })
+        vim.keymap.set("n", "<C-t>", function() harpoon.ui:select_menu_item({ tabedit = true }) end, { buffer = cx.bufnr })
+      end,
+    })
     vim.keymap.set('n', '<leader>jj', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[J] Quick menu', silent = true })
     vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end, { desc = 'Mark file', silent = true })
 
@@ -11,6 +23,7 @@ return {
     vim.keymap.set("n", "<A-d>", function() harpoon:list():select(2) end, { desc = 'Select item 2', silent = true })
     vim.keymap.set("n", "<A-s>", function() harpoon:list():select(3) end, { desc = 'Select item 3', silent = true })
     vim.keymap.set("n", "<A-a>", function() harpoon:list():select(4) end, { desc = 'Select item 4', silent = true })
+
   end,
 
 }
