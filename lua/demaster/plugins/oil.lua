dacba739dddbfd9ba325683b0f47dbbea6baa8a2
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-01-13 15:19:24 -0300

    feat(plugins): update floating window settings for oil.nvim

--- lua/demaster/plugins/oil.lua
+++ lua/demaster/plugins/oil.lua
@@ -13,6 +13,25 @@ return {
       buflisted = false,
       bufhidden = 'hide',
     },
+    float = {
+      -- Padding around the floating window
+      padding = 1,
+      max_width = 0.25,
+      max_height = 0.60,
+      border = 'rounded',
+      win_options = {
+        winblend = 0,
+      },
+      -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+      get_win_title = nil,
+      -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+      preview_split = 'below',
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      override = function(conf)
+        return conf
+      end,
+    },
   },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
