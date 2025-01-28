f9f9b6d5ca6597f69f8258969264cde4cdbf058a
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-01-28 17:12:34 -0300

    feat(plugins): installed obsidian.nvim

---
+++ lua/demaster/plugins/obsidian.lua
@@ -0,0 +1,31 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    templates = {
+        folder = "5 Templates",
+        date_format = "%Y-%m-%d-%a",
+        time_format = "%H:%M",
+    },
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/Notes/Personal",
+      },
+      {
+        name = "new-personal",
+        path = "~/Notes/twentieth-second-brain/source/content",
+      },
+      {
+        name = "work",
+        path = "~/Notes/Work",
+      },
+    },
+  },
+}
