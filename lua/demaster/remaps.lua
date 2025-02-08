5c70f532b92027e1a2403bc127d6c75303ff8d07
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-02-08 15:11:06 -0300

    feat(remaps): add remap to open a split terminal

--- lua/demaster/remaps.lua
+++ lua/demaster/remaps.lua
@@ -15,8 +15,13 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 --vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('t', '<C-w>n', '<C-\\><C-n>', {desc = 'Exit Terminal mode'})
-
+vim.keymap.set('t', '<C-w>n', '<C-\\><C-n>', { desc = 'Exit Terminal mode' })
+vim.keymap.set('n', '<leader>te', function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd 'J'
+  vim.api.nvim_win_set_height(0, 15)
+end, { desc = 'Exit Terminal mode' })
 
 --
 --  See `:help wincmd` for a list of all window commands
@@ -27,28 +32,26 @@ vim.keymap.set('t', '<C-w>n', '<C-\\><C-n>', {desc = 'Exit Terminal mode'})
 -- vim.keymap.set("n", "<leader>w", ":wincmd k<CR>", {desc = 'Move to window up', silent = true })
 -- vim.keymap.set("n", "<leader>d", ":wincmd l<CR>", {desc = 'Move to right window', silent = true })
 
-
-vim.keymap.set("n", "<C-j>", ":cnext<CR>", {desc = 'Go to next error in the C error List'})
-vim.keymap.set("n", "<C-k>", ":cprevious<CR>", {desc = 'Go to previous error in the C error List'})
+vim.keymap.set('n', '<C-j>', ':cnext<CR>', { desc = 'Go to next error in the C error List' })
+vim.keymap.set('n', '<C-k>', ':cprevious<CR>', { desc = 'Go to previous error in the C error List' })
 
 --Resize splits
-vim.keymap.set("n", "<leader>=", ":vertical resize +5<CR>", {desc = 'Resize vertical buffer +5', silent = true })
-vim.keymap.set("n", "<leader>-", ":vertical resize -5<CR>", {desc = 'Resize vertical buffer -5', silent = true })
+vim.keymap.set('n', '<leader>=', ':vertical resize +5<CR>', { desc = 'Resize vertical buffer +5', silent = true })
+vim.keymap.set('n', '<leader>-', ':vertical resize -5<CR>', { desc = 'Resize vertical buffer -5', silent = true })
 
 --Buffers List
-vim.keymap.set("n", "<Leader>bp", ":b# <CR>", {desc = '[B]uffers: Go to [P]revious file', silent = true })
+vim.keymap.set('n', '<Leader>bp', ':b# <CR>', { desc = '[B]uffers: Go to [P]revious file', silent = true })
 
 --jq json formating
-vim.keymap.set("v", "<Leader>vjq", ":.!jq<CR>", {desc = 'Visual format of Json text', silent = true })
+vim.keymap.set('v', '<Leader>vjq', ':.!jq<CR>', { desc = 'Visual format of Json text', silent = true })
 
 -- Reselect visual selection after indenting
-vim.keymap.set("v", "<", "<gv", {desc = '', silent = true })
-vim.keymap.set("v", ">", ">gv", {desc = '', silent = true })
+vim.keymap.set('v', '<', '<gv', { desc = '', silent = true })
+vim.keymap.set('v', '>', '>gv', { desc = '', silent = true })
 
 -- Easy insertion of trailing ; or , from instert mode
-vim.keymap.set("i", ";;", "<Esc>A;<Esc>", {desc = 'Insert a ; at the end of a line', silent = true })
-vim.keymap.set("i", ",,", "<Esc>A,<Esc>", {desc = 'Insert a , at the end of a line', silent = true })
-
+vim.keymap.set('i', ';;', '<Esc>A;<Esc>', { desc = 'Insert a ; at the end of a line', silent = true })
+vim.keymap.set('i', ',,', '<Esc>A,<Esc>', { desc = 'Insert a , at the end of a line', silent = true })
 
 --Never used these much, have to check for conflicts with kickstart
 --nnoremap("<Leader>cl", ":s///gn<CR>", {desc = '[C]ount ocurrences of last searched pattern in [L]ine'})
@@ -60,23 +63,22 @@ vim.keymap.set("i", ",,", "<Esc>A,<Esc>", {desc = 'Insert a , at the end of a li
 --
 --Greates remap ever
 --Delete to void what you have selected and paste your previous yanked stuff
-vim.keymap.set("v", "<leader>p", '"_dP', {desc = '', silent = true })
+vim.keymap.set('v', '<leader>p', '"_dP', { desc = '', silent = true })
 -- Copy to clipboard
-vim.keymap.set("v", "<leader>y", '"+y', {desc = '[Y]ank to clipboard', silent = true })
-vim.keymap.set("n", "<leader>Y", '"+yg_', {desc = '[Y]ank line to clipboard', silent = true })
-vim.keymap.set("n", "<leader>y", '"+y', {desc = '[Y]ank to clipboard', silent = true })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank to clipboard', silent = true })
+vim.keymap.set('n', '<leader>Y', '"+yg_', { desc = '[Y]ank line to clipboard', silent = true })
+vim.keymap.set('n', '<leader>y', '"+y', { desc = '[Y]ank to clipboard', silent = true })
 
 -- Paste from clipboard
-vim.keymap.set("n", "<leader>p", '"+p', {desc = '[P]aste from clipboard', silent = true })
-vim.keymap.set("n", "<leader>P", '"+P', {desc = '[P]aste from clipboard', silent = true })
-vim.keymap.set("v", "<leader>P", '"+P', {desc = '[P]aste from clipboard', silent = true })
-
+vim.keymap.set('n', '<leader>p', '"+p', { desc = '[P]aste from clipboard', silent = true })
+vim.keymap.set('n', '<leader>P', '"+P', { desc = '[P]aste from clipboard', silent = true })
+vim.keymap.set('v', '<leader>P', '"+P', { desc = '[P]aste from clipboard', silent = true })
 
 --Make the file executable right now!
-vim.keymap.set("n", "<leader>x", ":silent !chmod u+x %<CR>", {desc = 'Make file e[X]ecutable'})
+vim.keymap.set('n', '<leader>x', ':silent !chmod u+x %<CR>', { desc = 'Make file e[X]ecutable' })
 
 --
 --     Simple tab Splits
 --
-vim.keymap.set("n", "<leader>zo", ":tab split<CR>", { desc = '[O]pen buffer in a tab'})
-vim.keymap.set("n", "<leader>zp", ":tab close<CR>", { desc = 'CLose tab'})
+vim.keymap.set('n', '<leader>zo', ':tab split<CR>', { desc = '[O]pen buffer in a tab' })
+vim.keymap.set('n', '<leader>zp', ':tab close<CR>', { desc = 'CLose tab' })
