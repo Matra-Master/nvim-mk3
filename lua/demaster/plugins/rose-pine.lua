3e2f3cac0b0c84a9a43e92cd99df4c7d6778c8d4
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-03-06 11:01:46 -0300

    feat(themes): rose-pine adjustments

--- lua/demaster/plugins/rose-pine.lua
+++ lua/demaster/plugins/rose-pine.lua
@@ -9,17 +9,20 @@ return {
     -- setup here
     require('rose-pine').setup {
       transparent_background = true, -- disables setting the background color.
+      variant = "moon",
+      styles = {
+          transparency = true,
+      },
       highlight_groups = {
         TelescopeBorder = { fg = "highlight_high", bg = "none" },
         TelescopeNormal = { bg = "none" },
         TelescopePromptNormal = { bg = "base" },
         TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-        TelescopeSelection = { fg = "text", bg = "base" },
-        TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
       },
     }
     -- Load the colorscheme here.
-    vim.cmd.colorscheme 'rose-pine'
+    -- vim.cmd.colorscheme 'rose-pine'
+    vim.cmd.colorscheme 'rose-pine-moon'
 
   end,
 }
