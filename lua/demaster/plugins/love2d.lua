4a5dab3f3cedc3d5d3da5979e6d03ed50a7aadad
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-04-13 09:39:21 -0300

    feat(plugin): installed the Löve framework plugin

---
+++ lua/demaster/plugins/love2d.lua
@@ -0,0 +1,14 @@
+return {
+  "S1M0N38/love2d.nvim",
+  cmd = "LoveRun",
+  opts = {
+    path_to_love_bin = "~/personal/love-games/love.AppImage",
+    -- path_to_love_library = vim.fn.globpath(vim.o.runtimepath, "love2d/library"),
+    -- restart_on_save = false,
+  },
+  keys = {
+    { "<leader>v", ft = "lua", desc = "LÖVE" },
+    { "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+    { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+  },
+}
