096d6378cf8ac37410d88114d46f4436120af0f1
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-08-08 13:53:39 -0300

    fix: obsidian was bothering me

--- lua/demaster/plugins/obsidian.lua
+++ lua/demaster/plugins/obsidian.lua
@@ -1,32 +1,44 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
+  ft = 'markdown',
   dependencies = {
     -- Required.
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
   },
   opts = {
     disable_frontmatter = true,
     templates = {
-        folder = "5 Templates",
-        date_format = "%Y-%m-%d-%a",
-        time_format = "%H:%M",
+      folder = '5 Templates',
+      date_format = '%Y%m%d',
+      time_format = '%H%M',
     },
     workspaces = {
       {
-        name = "personal",
-        path = "~/Notes/Personal",
+        name = 'personal',
+        path = '~/Notes/Personal',
       },
       {
-        name = "new-personal",
-        path = "~/Notes/twentieth-second-brain/source/content",
+        name = 'new-personal',
+        path = '~/Notes/twentieth-second-brain/source/content',
       },
       {
-        name = "work",
-        path = "~/Notes/Work",
+        name = 'work',
+        path = '~/Notes/Work',
       },
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = '6 Dailies',
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = '%Y-%m-%d',
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = '%B %-d, %Y',
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { 'daily-notes' },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
     },
   },
 }
