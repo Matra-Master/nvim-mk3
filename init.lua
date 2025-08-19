d2eed336779a943613c38ca579d5a3636adf01af
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-08-19 10:10:20 -0300

    treesitter: markdown breaks for some reason on my ai buffers

--- init.lua
+++ init.lua
@@ -760,7 +760,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'hyprlang' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'query', 'vim', 'vimdoc', 'hyprlang' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
